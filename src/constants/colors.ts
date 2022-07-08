b500a855f1919adc8ffcf2c7263acc789c5b6564
Unekwe Francis <unekwenchekwube@gmail.com>
2022-07-08 19:28:33 +0100

    added safe args navigation dependency

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -6,7 +6,6 @@ const colors = {
   LightGray: ' rgba(217, 217, 217, 0.7)',
   inactiveTabColor: ' rgba(112, 102, 102, 1)',
   yellowBg: ' rgba(255, 254, 230, 1)',
-  // borderColor: 'rgba(151, 151, 151, 1)',
   borderColor: 'rgba(217, 217, 217, 0.7)',
   Background: '#F9F9FB',
   bgInputGray: '#F4F4F5',
@@ -15,7 +14,6 @@ const colors = {
   green50: 'rgba(232, 249, 244, 0.5)',
   green400: '#20C9B3',
   green700: '#48AC00',
-  // error: 'rgb(255,0,0)',
   error: '#c10718',
   disabledBtn: '#d7d7d7',
 };
